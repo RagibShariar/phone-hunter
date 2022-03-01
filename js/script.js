0520f8f30ee8d38da850acfebdf25f047205b593
RagibShariar <srragib@gmail.com>
2022-03-01 20:41:33 +0600

    20 items shown

--- js/script.js
+++ js/script.js
@@ -13,24 +13,27 @@ const searchPhone = () => {
 
 const displaySearchResult = (phones) => {
   const searchResult = document.getElementById('search-result');
-  console.log(phones);
-  // phones.forEach(phone => {
-  //     console.log(phone);
-  // });
-  for (const phone of phones) {
+  // console.log(phones);
+  for (const phone of phones.slice(0,20)) {
     // console.log(phone);
-
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
-        <div class="card">
-            <img src="${phone.image}" class="img-fluid w-50 m-auto" alt="...">
-            <div class="">
-                <h5 class="card-title mt-5">${phone.brand}</h5>
-                <p class="card-text">${phone.phone_name}</p>
-            </div>
-        </div>
+    <div class="card py-3">
+                <img src="${phone.image}" class="w-50 card-img-top img-fluid mx-auto pt-2" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">${phone.brand}</h5>
+                  <p class="card-text">${phone.phone_name}</p>
+                  <button onclick="loadPhoneDetail(${phone.slug})" class="btn btn-outline-dark">More Details</button>
+                </div>
+              </div>
     `;
     searchResult.appendChild(div);
   }
 };
+const loadPhoneDetail = (phoneId) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data));
+}
